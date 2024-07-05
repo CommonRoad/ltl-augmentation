303ffd917065b2598ed27e49ab94754c806a0643
Florian Lercher <florian.lercher@tum.de>
2024-07-05 18:11:09 +0200

    Change to library crate to silence warnings about unused methods

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod formula;
+pub mod interval;
+pub mod minimal_dnf;
+pub mod parser;
+pub mod rewrite;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-mod formula;
-mod interval;
-mod minimal_dnf;
-mod parser;
-mod rewrite;
+use mltl_simplification::parser::mltl_parser;
 
 fn main() {
+    let phi = mltl_parser::formula("a -> b U[3, 10] c").unwrap();
     println!("Hello, world!");
+    println!("This is an MLTL formula: {:?}", phi);
 }
