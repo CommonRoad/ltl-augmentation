82254d74aa6e5d46a9aac54f46711efa55e77c7e
Florian Lercher <florian.lercher@tum.de>
2024-09-13 10:24:51 +0200

    Add Python function for extracting relevant APs

--- src/clean/python/formula.rs
+++ src/clean/python/formula.rs
@@ -1,8 +1,12 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use pyo3::{exceptions::PyValueError, prelude::*};
 
-use crate::clean::formula::{nnf::NNFFormula, parser::mltl_parser};
+use crate::clean::{
+    formula::{nnf::NNFFormula, parser::mltl_parser},
+    sequence::Time,
+    sets::interval::Interval,
+};
 
 #[pyclass]
 pub struct Formula(pub NNFFormula);
@@ -23,6 +27,26 @@ impl Formula {
 
     fn __repr__(&self) -> PyResult<String> {
         Ok(format!("{:?}", self.0))
+    }
+
+    #[pyo3(signature = (end, start=0))]
+    fn relevant_aps(&self, end: Time, start: Time) -> HashMap<Time, Vec<String>> {
+        let interval = Interval::bounded(start, end).into();
+        let mut aps_with_time = HashMap::new();
+        for (ap, time_steps) in self.0.collect_aps_with_time() {
+            time_steps
+                .intersect(&interval)
+                .get_intervals()
+                .into_iter()
+                .flatten()
+                .for_each(|time| {
+                    aps_with_time
+                        .entry(time)
+                        .or_insert_with(Vec::new)
+                        .push(ap.name.to_string());
+                });
+        }
+        aps_with_time
     }
 }
 
