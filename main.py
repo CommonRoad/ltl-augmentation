af958396c9ab4b3a11fa5a24e4088ba9c01837fd
Florian Lercher <florian.lercher@tum.de>
2024-12-20 10:53:52 +0100

    Improve example scripts

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import ltl_augmentation as aug
 
 def main():
     formula = aug.Formula("(G l1 & b1 -> s1) & (G l2 & b2 -> s2)")
-    knowledge = aug.KnowledgeSequence({
-        0: (["l2", "b2"], ["b1"], [], []),
-        1: ([], [], [("b1", "b2"), ("s1", "s2")], [("l1", "l2")]),
-    })
+    print(f"Original formula:\n{formula}")
+    knowledge = aug.KnowledgeSequence(
+        {
+            0: (["l2", "b2"], ["b1"], [], []),
+            1: ([], [], [("b1", "b2"), ("s1", "s2")], [("l1", "l2")]),
+        }
+    )
     augmenter = aug.Augmenter(knowledge)
     augmented = augmenter.augment(formula)
-    print(augmented)
+    print(f"Augmented formula:\n{augmented}")
 
 
 if __name__ == "__main__":
